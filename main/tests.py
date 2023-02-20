54e6e8b53301ee18b9a84a051542890348e1e010
Murat Yıldırım <mybusinnes.21@gmail.com>
2023-02-20 04:18:21 +0300

    xxxxx

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
 
-# Create your tests here.
+from currency_converter import currency_converter
+from datetime import date
+c = currency_converter.CurrencyConverter()
+print(c.convert(100, 'USD', 'TRY' , date=date(2018 , 2 , 2)))
--- main/views.py
+++ main/views.py
@@ -11,6 +11,11 @@ from django.http.response import HttpResponse
 from django.http import HttpResponseRedirect
 from datetime import datetime
 from django.contrib.auth.decorators import login_required
+from currency_converter.currency_converter import CurrencyConverter
+from django.contrib import messages
+from imap_tools import MailBox
+
+
 
 
 def home(request):
@@ -36,53 +41,67 @@ def dbdownload(request , country):
 
 @login_required(login_url='login')
 def muhasebe(request):
+     
     pazarlar = Pazarlar.objects.get(KULLANICI = request.user)
     print(pazarlar)
     if request.method == 'POST':
-        if request.POST['countries'] == 'DE':
-            pazarlar.DE = True
-            pazarlar.DEMAIL = request.POST['mail']
-            pazarlar.DEPASSWORD = request.POST['apppassword']
-            pazarlar.save()
-        elif request.POST['countries'] == 'UK':
-            pazarlar.UK = True
-            pazarlar.UKMAIL = request.POST['mail']
-            pazarlar.UKPASSWORD = request.POST['apppassword']
-            pazarlar.save()
-        elif request.POST['countries'] == 'FR':
-            pazarlar.FR = True
-            pazarlar.FRMAIL = request.POST['mail']
-            pazarlar.FRPASSWORD = request.POST['apppassword']
-            pazarlar.save()
+        try :
+            mb = MailBox('imap.gmail.com').login(request.POST['mail'], request.POST['apppassword']) 
+            if request.POST['countries'] == 'DE':
+                pazarlar.DE = True
+                pazarlar.DEMAIL = request.POST['mail']
+                pazarlar.DEPASSWORD = request.POST['apppassword']
+                
+                pazarlar.save()
+            elif request.POST['countries'] == 'UK':
+                pazarlar.UK = True
+                pazarlar.UKMAIL = request.POST['mail']
+                pazarlar.UKPASSWORD = request.POST['apppassword']
+                pazarlar.save()
+            elif request.POST['countries'] == 'FR':
+                pazarlar.FR = True
+                pazarlar.FRMAIL = request.POST['mail']
+                pazarlar.FRPASSWORD = request.POST['apppassword']
+                pazarlar.save()
+            messages.success(request , "Kayıt başarı ile tamamlandı")
+        except Exception as e: 
+            messages.error(request , "Hatalı Bilgi Girişi !")
+            print(e)
         
     
-    
+    c = CurrencyConverter()
     #pdict = {'pazarlar' : pazarlar.EN}
     #keys = [i for i in pdict.keys()]
     fr = Fransa.objects.filter(KULLANICI = request.user)
     frSatis = sum([i.SATIS_FIYATI for i in fr])
     frFee = sum([i.AMAZON_FEE for i in fr])
     frProfit = sum([i.KAR for i in fr if i.KAR is not None])
-    frMaliyet = frSatis - frFee - frProfit
+    frAmazonMaliyet = sum([i.MALIYET for i in fr])
+    frDepoMaliyet = sum([i.DEPO_MALIYET for i in fr])
+    frMaliyet = frAmazonMaliyet + c.convert(frDepoMaliyet , 'EUR' , 'USD')
     
     de = Almanya.objects.filter(KULLANICI = request.user)
     deSatis = sum([i.SATIS_FIYATI for i in de])
     deFee = sum([i.AMAZON_FEE for i in de])
     deProfit = sum([i.KAR for i in de if i.KAR is not None])
-    deMaliyet = deSatis - deFee - deProfit
+    deAmazonMaliyet = sum([i.MALIYET for i in de])
+    deDepoMaliyet = sum([i.DEPO_MALIYET for i in de])
+    deMaliyet = deAmazonMaliyet + c.convert(deDepoMaliyet , 'EUR' , 'USD')
     
     uk = Ingiltere.objects.filter(KULLANICI = request.user)
     ukSatis = sum([i.SATIS_FIYATI for i in uk])
     ukFee = sum([i.AMAZON_FEE for i in uk])
     ukProfit = sum([i.KAR for i in uk if i.KAR is not None])
-    ukMaliyet = ukSatis - ukFee - ukProfit
+    ukAmazonMaliyet = sum([i.MALIYET for i in uk])
+    ukDepoMaliyet = sum([i.DEPO_MALIYET for i in uk])
+    ukMaliyet = ukAmazonMaliyet + c.convert(ukDepoMaliyet , 'GBP' , 'USD')
 
 
     
 
     return render(request , 'muhasebe.html', {'x' : pazarlar.get_items(frKazanc= frSatis - frFee , frProfit=frProfit , frMaliyet=  frMaliyet ,
                                                                         deKazanc= deSatis - deFee, deProfit= deProfit , deMaliyet=deMaliyet ,
-                                                                         ukKazanc=  ukSatis - ukFee , ukProfit=ukProfit , ukMaliyet=ukMaliyet)
+                                                                         ukKazanc=  ukSatis - ukFee , ukProfit=ukProfit , ukMaliyet=ukMaliyet),
                                                 })
 
 @login_required(login_url='login')
@@ -123,7 +142,7 @@ def ingiltere(request):
                 o_id = maliyet_form.data['order_id']
                 try:
                     update_cost(Ingiltere,o_id, p_cost, w_cost)
-                    update_profit(Ingiltere,o_id)
+                    update_profit(Ingiltere,o_id , currency1='GBP')
                 
                 except:
                     message = "Maliyet 0 Olamaz"
@@ -183,7 +202,7 @@ def almanya(request):
                 o_id = maliyet_form.data['order_id']
                 try:
                     update_cost(Almanya,o_id, p_cost, w_cost)
-                    update_profit(Almanya,o_id)
+                    update_profit(Almanya,o_id , currency1='EUR')
                 
                 except:
                     message = "Maliyet 0 Olamaz"
@@ -195,9 +214,9 @@ def almanya(request):
 
         elif 'update_order_list' in request.POST:
             if len(b) == 0 : 
-                mailparse(country=Almanya,user = request.user , email=pazarlar.UKMAIL , apppassword=pazarlar.UKPASSWORD , fdate=request.POST['date'])
+                mailparse(country=Almanya,user = request.user , email=pazarlar.DEMAIL , apppassword=pazarlar.DEPASSWORD , fdate=request.POST['date'])
             else:
-                mailparse(country=Almanya,user = request.user , email=pazarlar.UKMAIL , apppassword=pazarlar.UKPASSWORD , fdate=None)
+                mailparse(country=Almanya,user = request.user , email=pazarlar.DEMAIL , apppassword=pazarlar.DEPASSWORD , fdate=None)
     else:
         print("no form posted")
         form = UploadFileForm()
@@ -241,25 +260,26 @@ def fransa(request):
                 w_cost = maliyet_form.data['warehouse_cost']
                 o_id = maliyet_form.data['order_id']
 
-                try:
-                    
-                    update_cost(Fransa,o_id, p_cost, w_cost)
-                    update_profit(Fransa,o_id)
-                    return HttpResponseRedirect('../fransa')
                 
-                except:
-                    message = "Maliyet 0 Olamaz"
+                    
+                update_cost(Fransa,o_id, p_cost, w_cost)
+                update_profit(Fransa,o_id , 'EUR')
+                return HttpResponseRedirect('../fransa')
+                
+                """                    message = "Maliyet 0 Olamaz"
                     data = {
                     "info" : d,
                     'form' : form,
                     'message' : message
                     }
+                """
+
 
         elif 'update_order_list' in request.POST:
             if len(b) == 0 : 
-                mailparse(country=Fransa,user = request.user , email=pazarlar.UKMAIL , apppassword=pazarlar.UKPASSWORD , fdate=request.POST['date'])
+                mailparse(country=Fransa,user = request.user , email=pazarlar.FRMAIL , apppassword=pazarlar.FRPASSWORD , fdate=request.POST['date'])
             else:
-                mailparse(country=Fransa,user = request.user , email=pazarlar.UKMAIL , apppassword=pazarlar.UKPASSWORD , fdate=None)
+                mailparse(country=Fransa,user = request.user , email=pazarlar.FRMAIL , apppassword=pazarlar.FRPASSWORD , fdate=None)
             
             return HttpResponseRedirect('../fransa')
     else:
@@ -270,6 +290,8 @@ def fransa(request):
     return render(request , 'fransa.html' , data) 
 
 
+from currency_converter.currency_converter import CurrencyConverter
+
 def update_cost(country,order_id,product_cost,warehouse_cost):
     order = country.objects.get(SATICI_SIPARIS_NUMARASI = order_id)
     print(order)
@@ -277,11 +299,12 @@ def update_cost(country,order_id,product_cost,warehouse_cost):
     order.DEPO_MALIYET = warehouse_cost
     order.save()     
 
-def update_profit(country,order_id):
+def update_profit(country,order_id , currency1):
     order = country.objects.get(SATICI_SIPARIS_NUMARASI = order_id)
-    profit = round((order.SATIS_FIYATI + order.AMAZON_FEE) / 0.81 - order.MALIYET - order.DEPO_MALIYET / 0.81,2)
+    c = CurrencyConverter()
+    profit = round(c.convert(order.SATIS_FIYATI - order.AMAZON_FEE - order.DEPO_MALIYET , currency1 , 'USD') - order.MALIYET , 2)
     order.KAR = profit
-    order.YUZDELIK_KAR = round(order.KAR / (order.MALIYET + order.DEPO_MALIYET / 0.81),2)
+    order.YUZDELIK_KAR = round(order.KAR / (order.MALIYET + c.convert(order.DEPO_MALIYET , currency1 , 'USD') ),2)
     order.save()
 
 
